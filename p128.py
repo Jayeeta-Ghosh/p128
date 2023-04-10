6b26d1e91f0164be1c491b3badc5a892ad94689f
Jayeeta-Ghosh <84728726+Jayeeta-Ghosh@users.noreply.github.com>
2023-04-10 13:01:37 +0530

    Add files via upload

---
+++ p128.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+import requests
+import pandas as pd
+
+
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
+time.sleep(10)
+browser = webdriver.Edge("/Users/USER/Desktop/Python/chromedriver")
+
+
+soup = BeautifulSoup(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+Lum = []
+planets_data = []
+
+
+
+def scrape():
+    for i in range(1,2):
+        while True:
+            time.sleep(2)
+
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+
+            # Check page number    
+            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
+            
+            if current_page_num < i:
+                browser.find_element(browser.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+            elif current_page_num > i:
+                browser.find_element(browser.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
+            else:
+                break
+
+        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+            li_tags = ul_tag.find_all("li")
+            temp_list = []
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+
+            # Get Hyperlink Tag
+            hyperlink_li_tag = li_tags[0]
+
+            temp_list.append("https://exoplanets.nasa.gov"+ hyperlink_li_tag.find_all("a", href=True)[0]["href"])
+            
+            planets_data.append(temp_list)
+
+        browser.find_element(browser.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+
+        print(f"Page {i} scraping completed")
+
+
+# Calling Method
+scrape()
+page = requests.get(bright_stars_url)
+print(page)
+
+def scrape_more_data(hyperlink):
+    try:
+        page = requests.get(hyperlink)
+      
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        temp_list = []
+
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+          
+            for td_tag in td_tags:
+                try: 
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+                    
+        new_stars_data.append(temp_list)
+
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
